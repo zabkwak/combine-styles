63371a4f50248a6bba81545b2f47957c0838b7a3
Lukáš Mačuda <lukas.macuda@gmail.com>
2019-09-11 15:15:42 +0200

    using node-sass importer

--- src/extentsion-processors/sass.ts
+++ src/extentsion-processors/sass.ts
@@ -6,34 +6,38 @@ import Base from './base';
 
 export default class Sass extends Base {
 
-    public process(src: string, dest: string): Promise<void> {
-        return new Promise((resolve, reject) => {
-            fs.readFile(src, (err, buffer) => {
-                if (err) {
-                    reject(err);
-                    return;
-                }
-                const dir = path.dirname(src);
-                // TODO relative path to the node_modules
-                const data = buffer.toString().replace(/@import(.*)~/g, '@import$1./node_modules/');
-                sass.render({
-                    data,
-                    //file: src,
-                    includePaths: [dir],
-                }, (err, result) => {
-                    if (err) {
-                        reject(err);
-                        return;
-                    }
-                    fs.writeFile(dest, result.css, (err) => {
-                        if (err) {
-                            reject(err);
-                            return;
-                        }
-                        resolve();
-                    });
-                });
-            });
-        });
-    }
+	public process(src: string, dest: string): Promise<void> {
+		return new Promise((resolve, reject) => {
+			fs.readFile(src, (err, buffer) => {
+				if (err) {
+					reject(err);
+					return;
+				}
+				const dir = path.dirname(src);
+				// TODO relative path to the node_modules
+				const data = buffer.toString().replace(/@import(.*)~/g, '@import$1./node_modules/');
+				sass.render({
+					data,
+					//file: src,
+					includePaths: [dir],
+					importer(url, prev, done) {
+						url = url.replace(/~/, './node_modules/');
+						done({ file: url });
+					},
+				}, (err, result) => {
+					if (err) {
+						reject(err);
+						return;
+					}
+					fs.writeFile(dest, result.css, (err) => {
+						if (err) {
+							reject(err);
+							return;
+						}
+						resolve();
+					});
+				});
+			});
+		});
+	}
 }
